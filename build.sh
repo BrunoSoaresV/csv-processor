b177ff4fff4eeccf9fa68d0a52014406237f11a3
Bruno Soares Veríssimo <94240946+BrunoSoaresV@users.noreply.github.com>
2025-05-08 14:26:56 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ OUTPUT_FILE="libcsv.so"
 gcc -Wall -fPIC -c libcsv.c -o libcsv.o
 gcc -shared -o $OUTPUT_FILE libcsv.o
 
+# Compilar o programa de testes
+gcc -Wall -I. test_libcsv.c -L. -lcsv -o test_libcsv -Wl,-rpath=.
+
 # Limpar arquivos temporários
 rm -f libcsv.o
 
-echo "Build completed: $OUTPUT_FILE"
+echo "Build completed: $OUTPUT_FILE and test_libcsv"
